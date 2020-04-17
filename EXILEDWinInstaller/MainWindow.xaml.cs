2176b85a51024bf6e85c75559872e470c8550491
Jorge G <jorgg69@gmail.com>
2020-04-17 21:29:09 +0200

    tinybrains will thank me for this

--- EXILEDWinInstaller/MainWindow.xaml.cs
+++ EXILEDWinInstaller/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using File = System.IO.File;
+using System.Security;
 
 namespace EXILEDWinInstaller
 {
@@ -27,6 +29,32 @@ namespace EXILEDWinInstaller
 			get => multiAdminCheckBox.IsChecked ?? false || MultiAdminInstalled;
 		}
 		private bool MultiAdminInstalled => File.Exists(InstallDir + "MultiAdmin.exe");
+
+		public string HighestCreatedFolder
+		{
+			get
+			{
+				Stack<int> indexes = new Stack<int>();
+				string currentFolder = InstallDir;
+				for (int i = 0; i < currentFolder.Length; i++)
+				{
+					if (currentFolder[i] == '\\')
+					{
+						indexes.Push(i);
+					}
+				}
+				while (indexes.Count > 0)
+				{
+					currentFolder = currentFolder.Substring(0, indexes.Pop());
+					if (Directory.Exists(currentFolder))
+					{
+						return currentFolder + "\\";
+					}
+				}
+				return null;
+			}
+		}
+
 		public MainWindow()
 		{
 			// The constructor is where we check if EXILED is already installed.
@@ -35,10 +63,10 @@ namespace EXILEDWinInstaller
 				MessageBox.Show("The installer is already running.", "Already running.", MessageBoxButton.OK, MessageBoxImage.Error);
 				Application.Current.Shutdown();
 			}
-			
+
 			InitializeComponent();
 			InstallButton.Click += OnInstallButton;
-			exiledFound = File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\EXILED\\EXILED.dll"); 
+			exiledFound = File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\EXILED\\EXILED.dll");
 			string[] args = Environment.GetCommandLineArgs();
 			for (int i = 0; i < args.Length; i++)
 			{
@@ -75,8 +103,8 @@ namespace EXILEDWinInstaller
 						break;
 				}
 			}
-			FileNameTextBox.Text = InstallDir; 
-			
+			FileNameTextBox.Text = InstallDir;
+
 			RefreshInstallButton();
 			Instance = this;
 		}
@@ -95,7 +123,7 @@ namespace EXILEDWinInstaller
 			if (dlWindow != null) return;
 			if (CheckDirectory())
 			{
-				dlWindow = new DownloadWindow(MultiAdmin, InstallDir, 
+				dlWindow = new DownloadWindow(MultiAdmin, InstallDir,
 					testingReleaseCheckbox.IsChecked ?? false, mustDownload, shortcutButton.IsChecked ?? false);
 				dlWindow.ShowDialog();
 			}
@@ -156,30 +184,36 @@ namespace EXILEDWinInstaller
 			InstallButtonText.Inlines.Clear();
 			if (download)
 			{
-				Run run = new Run($"{(forceInstall.IsChecked ?? false ? "Update" : "Download")} SCP:SL and install ") {
+				Run run = new Run($"{(forceInstall.IsChecked ?? false ? "Update" : "Download")} SCP:SL and install ")
+				{
 					Foreground = new SolidColorBrush(Color.FromRgb(229, 229, 229))
 				};
 				InstallButtonText.Inlines.Add(run);
-				run = new Run("E") {
+				run = new Run("E")
+				{
 					Foreground = new SolidColorBrush(Color.FromRgb(214, 51, 48))
 				};
 				InstallButtonText.Inlines.Add(run);
-				run = new Run("XILED") {
+				run = new Run("XILED")
+				{
 					Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255))
 				};
 				InstallButtonText.Inlines.Add(run);
 			}
 			else
 			{
-				Run run = new Run(exiledFound ? "Update " : "Install ") {
+				Run run = new Run(exiledFound ? "Update " : "Install ")
+				{
 					Foreground = new SolidColorBrush(Color.FromRgb(229, 229, 229))
 				};
 				InstallButtonText.Inlines.Add(run);
-				run = new Run("E") {
+				run = new Run("E")
+				{
 					Foreground = new SolidColorBrush(Color.FromRgb(214, 51, 48))
 				};
 				InstallButtonText.Inlines.Add(run);
-				run = new Run("XILED") {
+				run = new Run("XILED")
+				{
 					Foreground = new SolidColorBrush(Color.FromRgb(255, 255, 255))
 				};
 				InstallButtonText.Inlines.Add(run);
@@ -190,8 +224,7 @@ namespace EXILEDWinInstaller
 			try
 			{
 				System.Diagnostics.Process.Start(@"https://www.github.com/galaxy119/EXILED");
-			}
-			catch
+			} catch
 			{
 				MessageBox.Show("We can't open your web explorer for you.\nVisit https://www.github.com/galaxy119/EXILED or search it in Google.", "Can't open link", MessageBoxButton.OK, MessageBoxImage.Error);
 			}
@@ -199,7 +232,7 @@ namespace EXILEDWinInstaller
 
 		private void CheckForceInstallToggle(object sender, RoutedEventArgs e) => RefreshInstallButton();
 
-		private void FocusCross(object sender, MouseEventArgs e) =>	cross.Source = new BitmapImage(new Uri(@"\images\focused_x.png", UriKind.Relative));
+		private void FocusCross(object sender, MouseEventArgs e) => cross.Source = new BitmapImage(new Uri(@"\images\focused_x.png", UriKind.Relative));
 
 		private void UnfocusCross(object sender, MouseEventArgs e) => cross.Source = new BitmapImage(new Uri(@"\images\unfocused_x.png", UriKind.Relative));
 
@@ -238,11 +271,32 @@ namespace EXILEDWinInstaller
 				FileNameTextBox.Text = InstallDir;
 				return false;
 			}
+
+			// The actual directory
 			if (FileNameTextBox.Text[FileNameTextBox.Text.Length - 1] != '\\')
 			{
 				FileNameTextBox.Text += '\\';
 			}
+			string oldDir = InstallDir;
 			InstallDir = FileNameTextBox.Text;
+
+			// Get the highest folder to check for permissions
+			string upmostParentFolder = HighestCreatedFolder;
+			if (HighestCreatedFolder == null)
+			{
+				MessageBox.Show("Invalid path. Please introduce a valid path\n(Example of a valid path: C:\\SCPSL\\MyServer", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+				FileNameTextBox.Text = InstallDir = oldDir;
+			}
+			try
+			{
+				File.Create(upmostParentFolder + "test.txt");
+				File.Delete(upmostParentFolder + "test.txt");
+			} catch 
+			{
+				MessageBox.Show("ERROR: You can't install your server in: " + upmostParentFolder + ".\r\n\r\nIf you feel this is an error, make the folder from the Windows Explorer and select it here.", "Access denied.", MessageBoxButton.OK, MessageBoxImage.Error);
+				FileNameTextBox.Text = InstallDir = oldDir;
+			}
+
 			RefreshInstallButton();
 			return true;
 		}
@@ -264,8 +318,7 @@ namespace EXILEDWinInstaller
 			try
 			{
 				System.Diagnostics.Process.Start(@"https://github.com/RogerFK/EXILED-Windows-Installer");
-			}
-			catch
+			} catch
 			{
 				MessageBox.Show("We can't open your web explorer for you.\nVisit https://github.com/RogerFK/EXILED-Windows-Installer or search it in Google.", "Can't open link", MessageBoxButton.OK, MessageBoxImage.Error);
 			}
